1218e8a0bbcc323eba80e9cc2532cd95e9a56ae1
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-08-27 21:46:57 +0500

    added fetch json

---
+++ script.js
@@ -0,0 +1,23 @@
+fetch("products.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (products) {
+    let out = "";
+    for (let product of products) {
+      out += `
+        <div class="product">
+          <img src="${product.image}" alt="${product.description}">
+          <p class="title">${product.title}</p>
+          <p class="description">${product.description}</p>
+          <p class="price">
+            <span>${product.price}</span>
+            <span>€</span>
+          </p>
+          <p class="cart">Add to cart <i class="bx bx-cart-alt"></i></p>
+        </div>
+      `;
+    }
+
+    document.querySelector(".products").innerHTML = out;
+  });
